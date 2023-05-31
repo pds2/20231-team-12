a6c9e7843f713c885fbd710718ce28d6975618ba
pilllla <carol.moyapilla@gmail.com>
2023-05-30 22:48:02 -0300

    definicao perfil usuario

--- include/perfil_usuario.hpp
+++ include/perfil_usuario.hpp
@@ -1,4 +1,14 @@
 #ifndef PERFIL_USUARIO_H
 #define PERFIL_USUARIO_H
 
+#include <iostream>
+
+class Perfil
+{
+private:
+    // esses 3 precisam estar relacionados no banco de dados
+    unsigned int _ID_perfil_usuario;
+    std::string _email_perfil_usuario;
+};
+
 #endif
