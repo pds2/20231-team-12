6a25c252def06a757823d8ae09fb0dc9db6aaccd
Raissalayla16 <raissalayla16@gmail.com>
2023-06-09 20:15:07 -0300

    atualizacao

--- include/exemplar.h
+++ include/exemplar.h
@@ -6,18 +6,20 @@
 class Exemplar : public Acervo {
 public:
     Exemplar(std::string autor, int anoPublicacao, std::string titulo, std::string genero, float codigo,
-             bool emprestado, int dataAquisicao, int codigoEspecifico);
+             bool emprestado, int dataAquisicao, int codigoEspecifico, int dataDevolucao);
 
     // Métodos getters
     bool isEmprestado() const;
     int getDataAquisicao() const;
     int getCodigoEspecifico() const;
+    int getDataDevolucao() const;
 
 
 private:
     bool emprestado;
     int dataAquisicao;
     int codigoEspecifico;
+    int dataDevolucao;
 };
 
 #endif  // EXEMPLAR_H
--- src/exemplar.cpp
+++ src/exemplar.cpp
@@ -1,13 +1,16 @@
 #include "exemplar.h"
 
 Exemplar::Exemplar(std::string autor, int anoPublicacao, std::string titulo, std::string genero, float codigo,
-                   bool emprestado, int dataAquisicao, int codigoEspecifico)
+                   bool emprestado, int dataAquisicao, int codigoEspecifico, int dataDevolucao)
     : Acervo(autor, anoPublicacao, titulo, genero, codigo), emprestado(emprestado), dataAquisicao(dataAquisicao),
-      codigoEspecifico(codigoEspecifico) {
+      codigoEspecifico(codigoEspecifico), dataDevolucao(dataDevolucao) {
 }
 
 bool Exemplar::isEmprestado() const {
     return emprestado;
+    if (emprestado == true){
+        getDataDevolucao();
+    }
 }
 
 int Exemplar::getDataAquisicao() const {
@@ -18,3 +21,6 @@ int Exemplar::getCodigoEspecifico() const {
     return codigoEspecifico;
 }
 
+int Exemplar::getDataDevolucao() const{
+    return dataDevolucao;
+}
