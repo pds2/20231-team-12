b4688de493e3af474cc23f4edf08fcce598d19c0
Carolina Pilla <carol.moyapilla@gmail.com>
2023-06-09 13:20:34 -0300

    mudancas em bibliotecario, get papel usuario nao é mais virtual

--- src/bibliotecario.cpp
+++ src/bibliotecario.cpp
@@ -5,17 +5,20 @@ Papel_do_usuario Bibliotecario::set_papel_usuario()
     return BIBLIOTECARIO;
 }
 
-Papel_do_usuario Bibliotecario::get_papel_usuario()
-{
-    return papel;
-}
-
 void Bibliotecario::adicionar_acervo(Acervo acervo_novo)
 {
 }
 
+void Bibliotecario::adicionar_exemplar(Exemplar exemplar_novo){
+
+}
+
 void Bibliotecario::remover_acervo(Acervo acervo_deletado)
 {
+}
+
+void Bibliotecario::remover_exemplar(Exemplar exemplar_deletado){
+    
 }
 
 void Bibliotecario::emprestimo_de_exemplar(Exemplar exemplar_emprestado)
