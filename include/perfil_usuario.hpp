f53cfe02354d5637398e4e9e96cfd9910cd9bb76
Carolina Pilla <carol.moyapilla@gmail.com>
2023-06-09 12:39:58 -0300

    implementacao de bibliotecario, mudanca em papeis de usuario, perfil_usuario como classe inacessivel fora das herdadas

--- include/perfil_usuario.hpp
+++ include/perfil_usuario.hpp
@@ -4,7 +4,16 @@
 #include <iostream>
 
 // TODOS os tipos de usuarios tem os mesmos atributos. o que muda sao os metodos que cada um pode fazer/tem acesso
-// nao sei onde a gente usaria virtual e override, talvez para retornar livros visiveis para bibliotecarios mas ocultos pra alunos?
+// nao sei onde a gente usaria virtual e override, talvez para retornar livros visiveis para bibliotecarios mas ocultos
+// pra alunos?
+
+enum Papel_do_usuario
+{
+    BIBLIOTECARIO,
+    ADMIN,
+    ALUNO
+};
+
 class Perfil_usuario
 {
 private:
@@ -13,17 +22,22 @@ private:
     std::string _email_perfil_usuario;
     // senha só numeros, se for negativo lanca um erro
     int _senha_pefil_usuario;
+    Papel_do_usuario _papel;
 
 public:
     // default, para testes
     Perfil_usuario();
 
-    // só da pra criar um usuario com os 3 atributos juntos, nunca pode faltar um
-    Perfil_usuario(unsigned int, std::string, int);
+    // só da pra criar um usuario com os 4 atributos juntos, nunca pode faltar um
+    Perfil_usuario(unsigned int, std::string, int, Papel_do_usuario);
 
     unsigned int get_ID_perfil_usuario();
     std::string get_email_perfil_usuario();
     int get_senha_perfil_usuario();
+    // essa é virtual pura, cada classe que herda vai declarar uma coisa diferente
+    // nao pode criar um usuario sem papel
+    virtual Papel_do_usuario set_papel_usuario() = 0;
+    virtual Papel_do_usuario get_papel_usuario() = 0;
 
     // destrutor
     ~Perfil_usuario();
