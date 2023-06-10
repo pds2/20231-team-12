5a5ba665b0ffe81f05b4e057f79ef32e35adafff
Layla Raissa <72211908+Raissalayla16@users.noreply.github.com>
2023-06-09 22:13:08 -0300

    Revert "correcao de erros"

--- include/acervo.h
+++ include/acervo.h
@@ -7,7 +7,7 @@ using namespace std;
 class Acervo {
 public:
     //Construtor
-    Acervo(string autor, int anoPublicacao, string titulo, string genero,float codigo);
+    Acervo(string autor, int anoPublicacao, string titulo, string genero,string codigo);
     virtual ~Acervo();
 
     //getters
@@ -15,14 +15,14 @@ public:
     int getAnoPublicacao() const;
     string getTitulo() const;
     string getGenero() const;
-    float getCodigo() const;
+    string getCodigo() const;
 
     //setters
-    /*string setAutor() const;
+    string setAutor() const;
     int setAnoPublicacao() const;
     string setTitulo() const;
     string setGenero() const;
-    float setCodigo() const;*/
+    string setCodigo() const;
 
 
 private:
@@ -30,7 +30,7 @@ private:
     int anoPublicacao;
     string titulo;
     string genero;
-    float codigo;
+    string codigo;
 };
 
 #endif  // ACERVO_H
