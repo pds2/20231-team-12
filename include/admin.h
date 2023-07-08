2efcb9cc3e913259ea404cb45b3ce1a31974ebd1
pilllla <carol.moyapilla@gmail.com>
2023-07-08 18:03:14 -0300

    inclusao de third party e outras mudancas

--- include/admin.h
+++ include/admin.h
@@ -6,30 +6,41 @@
 #include "aluno.h"
 #include "bibliotecario.hpp"
 
-class email_ja_cadastrado_e {};
-class id_invalido_e {};
-class id_nao_existe_e {};
-class id_ja_cadastrado_e {};
-class papel_invalido_e {};
+class email_ja_cadastrado_e
+{
+};
+class id_invalido_e
+{
+};
+class id_nao_existe_e
+{
+};
+class id_ja_cadastrado_e
+{
+};
+class papel_invalido_e
+{
+};
 
-class Admin : public Perfil_usuario {
-    public:
-        /*
-         * Constroi um admin.
-         */
-        Admin(unsigned int id, std::string email, int senha, Papel_do_usuario papel);
-        /*
-         * @brief Cadastra um novo usuário no sistema.
-         */
-        void adicionar_usuario(unsigned int id, std::string email, int senha, Papel_do_usuario papel);
-        /*
-         * @brief Deleta um usuario pelo id.
-         */
-        void deletar_usuario(unsigned int id);
-        /*
-         * Destroi o admin.
-         */
-        ~Admin();
+class Admin : public Perfil_usuario
+{
+public:
+    /*
+     * Constroi um admin.
+     */
+    Admin(unsigned int id, std::string email, int senha, Papel_do_usuario papel);
+    /*
+     * @brief Cadastra um novo usuário no sistema.
+     */
+    void adicionar_usuario(unsigned int id, std::string email, int senha, Papel_do_usuario papel);
+    /*
+     * @brief Deleta um usuario pelo id.
+     */
+    void deletar_usuario(unsigned int id);
+    /*
+     * Destroi o admin.
+     */
+    ~Admin();
 };
 
 #endif
--- tests/test_exemplar.cpp
+++ tests/test_exemplar.cpp
@@ -1,17 +1,16 @@
-//#include "../include/exemplar.h"
+// #include "../include/exemplar.h"
 
+// #include "../third_party/doctest.h"
 
-//#include "../third_party/doctest.h"
+// #include "exemplar.h"
 
-//#include "exemplar.h"
-
-//#include "doctest.h"
+// #include "doctest.h"
 
 #include "../include/exemplar.h"
 #include "../third_party/doctest.h"
 
-
-TEST_CASE("Teste de cálculo da data de devolução") {
+TEST_CASE("Teste de cálculo da data de devolução")
+{
     // Cria um exemplar com data de aquisição fixa (04/07/2023)
     Exemplar exemplar("Autor", 2023, "Título", "Gênero", 1.0, false, 20230704, 1, 0);
 
@@ -22,45 +21,34 @@ TEST_CASE("Teste de cálculo da data de devolução") {
     CHECK(dataDevolucaoCalculada == 20230804);
 }
 
-TEST_CASE("Exemplar Calcula Data Devolucao") {
-    Exemplar exemplar("Autor", 2021, "Titulo", "Genero", 12345.67, false, 20230617, 1, 0); //data aquisição 17/06/2023
+TEST_CASE("Exemplar Calcula Data Devolucao")
+{
+    Exemplar exemplar("Autor", 2021, "Titulo", "Genero", 12345.67, false, 20230617, 1, 0); // data aquisição 17/06/2023
 
     REQUIRE(exemplar.calculaDataDevolucao() == 20230717); // data devolução 17/07/2023
 }
 
-TEST_CASE("Exemplar Calcula Data Devolucao") {
-    Exemplar exemplar("Autor", 2021, "Titulo", "Genero", 12345.67, false, 20240219, 1, 0); //data aquisição 17/06/2023
+TEST_CASE("Exemplar Calcula Data Devolucao")
+{
+    Exemplar exemplar("Autor", 2021, "Titulo", "Genero", 12345.67, false, 20240219, 1, 0); // data aquisição 17/06/2023
 
     REQUIRE(exemplar.calculaDataDevolucao() == 20240319); // data devolução 19/03/2023 - passou
 }
 
-TEST_CASE("Exemplar Calcula Data Devolucao") {
-    Exemplar exemplar("Autor", 2021, "Titulo", "Genero", 12345.67, false, 20240218, 1, 0); //data aquisição 17/06/2023
+TEST_CASE("Exemplar Calcula Data Devolucao")
+{
+    Exemplar exemplar("Autor", 2021, "Titulo", "Genero", 12345.67, false, 20240218, 1, 0); // data aquisição 17/06/2023
 
     REQUIRE(exemplar.calculaDataDevolucao() == 20240319); // data devolução 18/03/2024 - deu errado e era para dar errado - passou
 }
 
-//deu errado - verificar
-TEST_CASE("Exemplar Calcula Multa com 3 dia de atraso") {
+// deu errado - verificar
+TEST_CASE("Exemplar Calcula Multa com 3 dia de atraso")
+{
     Exemplar exemplar("Autor", 2021, "Titulo", "Genero", 12345.67, false, 20230617, 1, 0);
 
     CHECK(exemplar.calculaMulta() == 3);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 /*
 g++ -std=c++20 test_exemplar.cpp exemplar.cpp -o test_exemplar -Ilibs
