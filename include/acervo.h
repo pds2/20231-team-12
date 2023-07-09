b17f1bc27b04be3302d72c88ca7ae30914ad7116
pilllla <carol.moyapilla@gmail.com>
2023-07-08 21:57:31 -0300

    ??

--- include/acervo.h
+++ include/acervo.h
@@ -1,17 +1,10 @@
 #ifndef ACERVO_H
 #define ACERVO_H
 
+#include "bancodedados.h"
 #include <string>
 
-enum CODIGOS_GENEROS_ACERVO
-{
-    MATEMATICA,
-    FISICA,
-    ARTE,
-    PROGRAMACAO,
-    CULINARIA,
-    HISTORIA
-};
+// criar os codigos especificos de cada genero em um arquivo separado
 
 // ACERVO NAO É virtual (pura), da pra criar um acervo sem ter nenhum exemplar (meio estranho mas ta certo)
 class Acervo
@@ -20,20 +13,24 @@ private:
     std::string _autor;
     int _ano_publicacao;
     std::string _titulo;
-    CODIGOS_GENEROS_ACERVO _genero;
+    int _genero;
     int _codigo;
 
 public:
     // Construtor
-    Acervo(std::string autor, int ano_publicacao, std::string titulo, CODIGOS_GENEROS_ACERVO genero, int codigo);
+    Acervo(std::string autor, int ano_publicacao, std::string titulo, int genero, int codigo);
     ~Acervo();
 
     // getters
     std::string get_autor() const;
     int get_ano_publicacao() const;
     std::string get_titulo() const;
-    CODIGOS_GENEROS_ACERVO get_genero() const;
+    int get_genero() const;
     int get_codigo() const;
+
+protected:
+    const char *file = "biblioteca.db";
+    BD bibdados;
 };
 
 #endif // ACERVO_H
