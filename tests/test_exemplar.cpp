a0592bfe8266e5fe20c097ec98055154ad947519
Raissalayla16 <raissalayla16@gmail.com>
2023-07-10 23:06:20 -0300

    atualizaçãoes

--- tests/test_exemplar.cpp
+++ tests/test_exemplar.cpp
@@ -10,7 +10,7 @@
 
 TEST_CASE("Teste de cálculo da data de devolução") {
     // Cria um exemplar com data de aquisição fixa (04/07/2023)
-    Exemplar exemplar("Autor", 2023, "Título", "Gênero", 12, false, 1);
+    Exemplar exemplar("Autor", 2023, "Título", "Gênero", 172, false, 12412023);
 
       exemplar.setDataEmprestimo(20230704);
     // Calcula a data de devolução do exemplar
@@ -22,7 +22,7 @@ TEST_CASE("Teste de cálculo da data de devolução") {
 
 
 TEST_CASE("Exemplar Calcula Data Devolucao") {
-    Exemplar exemplar("Autor", 2021, "Titulo", "Genero", 12345, false, 1); 
+    Exemplar exemplar("Autor", 2021, "Titulo", "Genero", 694, false, 34572021); 
 
     exemplar.setDataEmprestimo(20230617); //data aquisição 17/06/2023
 
@@ -31,15 +31,16 @@ TEST_CASE("Exemplar Calcula Data Devolucao") {
 
 
 TEST_CASE("Exemplar Calcula Data Devolucao") {
-    Exemplar exemplar("Autor", 2021, "Titulo", "Genero", 12345, false, 1); 
+    Exemplar exemplar("Autor", 2021, "Titulo", "Genero", 234, false, 12792021); 
 
     exemplar.setDataEmprestimo(20240219);//data aquisição 19/02/2024
 
     CHECK(exemplar.calculaDataDevolucaoSistema() == 20240319); // data devolução 19/03/2023 - passou
 }
 
+
 TEST_CASE("Exemplar Calcula Data Devolucao") {
-    Exemplar exemplar("Autor", 2021, "Titulo", "Genero", 12345, false, 1); //data aquisição 17/06/2023
+    Exemplar exemplar("Autor", 2021, "Titulo", "Genero", 845, false, 14352021); //data aquisição 17/06/2023
 
   exemplar.setDataEmprestimo(20240218); //data aquisição 18/02/2024
 
@@ -51,7 +52,7 @@ TEST_CASE("Exemplar Calcula Data Devolucao") {
 //logo abaixo há um igual porem pegando os dias passados desde o dia do teste como se ele tivesse aumentando a multa ainda a mais no assertion a cada dia
 /*
 TEST_CASE("Exemplar - Teste do método getMulta - Com multa") {
-    Exemplar exemplar("Autor", 2021, "Título", "Gênero", 1234, false,  1);
+    Exemplar exemplar("Autor", 2021, "Título", "Gênero", 123, false,  76592021);
     // Data de aquisição: 05/06/2023
     // Data de devolução: 08/07/2023 (1 mês e 3 dias após aquisição)
         exemplar.setDataEmprestimo(20230605);
@@ -66,7 +67,7 @@ TEST_CASE("Exemplar - Teste do método getMulta - Com multa") {
 //Pegando os dias passados desde o dia do teste como se ele tivesse aumentando a multa ainda a mais no assertion a cada dia
 TEST_CASE("Exemplar - Teste do método getMulta e método Calcula Multa - Com multa de 3 reais")
 {
-    Exemplar exemplar("Autor", 2021, "Título", "Gênero", 1234, false, 1);
+    Exemplar exemplar("Autor", 2021, "Título", "Gênero", 124, false, 14782021);
     // Data de aquisição: 05/06/2023
     // Data de devolução: 08/07/2023 (1 mês e 4 dias após aquisição)
     exemplar.setDataEmprestimo(20230605);
@@ -81,6 +82,7 @@ TEST_CASE("Exemplar - Teste do método getMulta e método Calcula Multa - Com mu
     dataFutura.tm_mon = 6;   // Mês: 7 - julho,  pois vai de  0 - 11 meses ao invés de 1 a 12
     dataFutura.tm_mday = 8;  // Dia: 8
 
+    //métodos da biblioteca c.time
     std::time_t dataFuturaTimestamp = std::mktime(&dataFutura);
     std::time_t duracaoTimestamp = dataAtual - dataFuturaTimestamp;
 
@@ -94,7 +96,7 @@ TEST_CASE("Exemplar - Teste do método getMulta e método Calcula Multa - Com mu
 //Teste passou, dia 08/07/2023 deve ficar em comentário, pois ele vai mudando conforme a data e vai gerar multa
 /*
 TEST_CASE("Exemplar Calcula Multa - test do getMulta - Sem multa") {
-    Exemplar exemplar("Autor", 2021, "Titulo", "Genero", 12345, false, 1);
+    Exemplar exemplar("Autor", 2021, "Titulo", "Genero", 235, false, 13452021);
     // Data de aquisição: 09/06/2023
     // Data de devolução: 09/07/2023 (1 mês após aquisição)
     exemplar.setDataEmprestimo(20230609);
@@ -107,16 +109,16 @@ TEST_CASE("Exemplar Calcula Multa - test do getMulta - Sem multa") {
 */
 
 TEST_CASE("Teste de métodos getters e construtor") {
-    Exemplar exemplar("Kiera Cass", 2008, "A seleção", "Romance", 0124, true, 3);
+    Exemplar exemplar("Kiera Cass", 2008, "A seleção", "Romance", 012, true, 32472008);
 
     // Verifica se os valores fornecidos no construtor são armazenados corretamente
     CHECK(exemplar.getAutor() == "Kiera Cass");
     CHECK(exemplar.getAnoPublicacao() == 2008);
     CHECK(exemplar.getTitulo() == "A seleção");
     CHECK(exemplar.getGenero() == "Romance");
-    CHECK(exemplar.getCodigo() == 0124);
+    CHECK(exemplar.getCodigo() == 012);
     CHECK(exemplar.isEmprestado() == true);
-    CHECK(exemplar.getCodigoEspecifico() == 3);
+    CHECK(exemplar.getCodigoEspecifico() == 32472008);
 }
 
 
